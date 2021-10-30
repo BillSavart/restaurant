4c2b4bc4540a4bd4ff20b77b71ddf2d93c1a7913
BillSavart <energyy0512@gmail.com>
2021-10-30 19:49:40 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        googleComment: ['EB Garamond'],
+        googleTitle: ['Abril Fatface'],
+        webTitle: ['Caveat']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
